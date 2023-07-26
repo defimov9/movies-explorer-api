7cfa6d16b6fc7dfe99a0867aeb7ccdc60fe8d711
Denis Efimov <defimov9@gmail.com>
2023-07-26 15:50:44 +0500

    feat: добавить логгер запросов и ошибок

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const helmet = require('helmet');
+const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const router = require('./routes/index');
 
 const {
   PORT,
-  MONGODB_URL = 'mongodb://localhost:27017/bitfilmsdb',
+  MONGODB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb',
 } = process.env;
 
 const app = express();
@@ -14,7 +18,14 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 
-mongoose.set('strictQuery', true);
-mongoose.connect(MONGODB_URL, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URL, {
+  useNewUrlParser: true,
+});
+
+app.use(requestLogger);
+
+app.use(router);
+
+app.use(errorLogger);
 
 app.listen(PORT);
